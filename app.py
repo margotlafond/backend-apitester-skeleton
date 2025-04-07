4a5f0d1d5e04b6431c63ea6ac5fe48508144fa27
Margot Lafond <margot.lafond@etu.minesparis.psl.eu>
2025-04-07 17:37:00 +0200

    ça marche mais pas avec le testeur, seulement avec les liens à la main

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ data = pl.Path(__file__).parent.absolute() / 'data'
 # Charger les données CSV
 associations_df = pd.read_csv(data / 'associations_etudiantes.csv')
 evenements_df = pd.read_csv(data / 'evenements_associations.csv')
+print(associations_df["type"] == "BDA")
+print(associations_df[associations_df['type'] == 'BDA'])  # noqa: E721
 
 ## Vous devez ajouter les routes ici :
 @app.route("/api/alive", methods=['GET'])
@@ -24,7 +26,7 @@ def alive_function():
 
 @app.route("/api/associations", methods=['GET'])
 def assos_function():
-    response = make_response(jsonify(associations_df[["id", "nom"]].values.tolist()), 200)
+    response = make_response(jsonify(associations_df["id"].values.tolist()), 200)
     return response
 
 @app.route("/api/associations/<int:id>", methods=['GET'])
@@ -34,6 +36,21 @@ def assos_details(id):
     else:
         response = make_response(jsonify({"error": "Association not found"}), 404)
     return response
+
+@app.route("/api/associations/<int:id>/evenements", methods=['GET'])
+def assos_evts(id):
+    if id >= 1 and id <=4 :
+        response = make_response(jsonify(evenements_df.loc[id-1, "description"]), 200)
+    else:
+        response = make_response(jsonify({"error": "Association not found"}), 404)
+    return response
+
+@app.route("/api/associations/type/<type>", methods=['GET'])
+def assos_type(type):
+    df_assos = associations_df[associations_df['type'] == type]
+    response = make_response(jsonify(df_assos["nom"].tolist()), 200)
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=False)
